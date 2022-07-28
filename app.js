90d1a98323fbc193906dba02a0f1e701ce27d04c
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-07-28 09:48:19 +0500

    Traffic-Signals

---
+++ app.js
@@ -0,0 +1,12 @@
+function stop() {
+    var stopImg = document.getElementById("stop-images");
+    stopImg.src = "./images/1.png";
+}
+function getReady() {
+    var stopImg = document.getElementById("stop-images");
+    stopImg.src = "./images/2.png";
+}
+function go() {
+    var stopImg = document.getElementById("stop-images");
+    stopImg.src = "./images/3.png";
+}
